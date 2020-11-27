7417c5707e8d804ec26d30540339c7bc6808cc34
nacitekir <50867176+nacitekir@users.noreply.github.com>
2020-11-27 09:51:06 +0300

    Add files via upload

--- frontend.py
+++ frontend.py
@@ -1,8 +1,106 @@
 import sys
 from PyQt5 import QtWidgets,QtGui
+import sqlite3
+
+
+class Member():
+    def __init__(self, ad, parola):
+        self.ad = ad
+        self.parola = parola
 
 
 class Pencere(QtWidgets.QWidget):
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.init_ui()
+        self.baglanti_olustur()
+
+    def baglanti_olustur(self):
+        self.con = sqlite3.connect("database.db")
+
+        self.cursor = self.con.cursor()
+
+        self.cursor.execute("Create Table If not Exists members (kullanici_adi TEXT,parola TEXT)")
+        self.con.commit()
+
+    def init_ui(self):
+        self.kullanici_adi = QtWidgets.QLineEdit()
+        self.parola = QtWidgets.QLineEdit()
+        self.parola.setEchoMode(QtWidgets.QLineEdit.Password)  # parolanın görünmez olmasını sağlıyor.
+        self.yazi_alani = QtWidgets.QLabel("")
+        self.giris = QtWidgets.QPushButton("GİRİŞ")
+        self.kayit = QtWidgets.QPushButton("KULLANICI KAYDET")
+
+        v_box = QtWidgets.QVBoxLayout()
+        v_box.addWidget(self.kullanici_adi)
+        v_box.addWidget(self.parola)
+        v_box.addWidget(self.yazi_alani)
+        v_box.addWidget(self.giris)
+        v_box.addWidget(self.kayit)
+        v_box.addStretch()
+
+        h_box = QtWidgets.QHBoxLayout()
+        h_box.addStretch()
+        h_box.addLayout(v_box)
+        h_box.addStretch()
+
+        self.setLayout(h_box)
+        self.setWindowTitle("Kullanıcı Girişi")
+
+        self.giris.clicked.connect(self.oku)
+        self.kayit.clicked.connect(self.oku)
+        self.show()
+
+    def oku(self):
+
+        isim = self.kullanici_adi.text()
+        par = self.parola.text()
+        member = Member(isim, par)
+
+        sender = self.sender()
+
+        if sender.text() == "KAYDET":
+            self.save(member)
+        else:
+            self.login(member)
+
+    def login(self, member):
+
+        sorgu = "Select * from members where kullanici_adi =? and parola= ?"
+        self.cursor.execute(sorgu, (member.ad, member.parola))
+        liste = self.cursor.fetchall()
+
+        if len(liste) == 0:
+            self.yazi_alani.setText("Yanlış giriş yaptınız!\nLütfen tekrar deneyiniz.")
+        else:
+            self.yazi_alani.setText("Başarılı şekilde giriş yaptınız.\n" + "Hoşgeldiniz " + member.ad)
+            self.pen = Base()
+
+
+
+    def save(self, member):
+
+        sorgu = "Select * from members where kullanici_adi =? "
+        self.cursor.execute(sorgu, (member.ad,))
+        liste = self.cursor.fetchall()
+
+        if len(liste) == 0:
+            self.cursor.execute("Insert into members Values(?,?)", (member.ad, member.parola))
+            self.con.commit()
+
+            self.yazi_alani.setText("Kayıt başarılı şekilde oluşturulmuştur.")
+        else:
+            self.yazi_alani.setText("Bu isim ile kayıtlı kullanıcı bulunmaktadır.")
+
+
+
+
+
+
+class Base(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.init_ui()
@@ -95,13 +193,9 @@ class Pencere1(QtWidgets.QWidget):
         v_box.addStretch()
         self.setLayout(v_box)
 
-        self.buton.clicked.connect(self.cikis)
-
 
         self.show()
 
-    def cikis(self):
-        QtWidgets.qApp.quit()
 
 class Pencere2(QtWidgets.QWidget):
     def __init__(self):
@@ -144,9 +238,12 @@ class Pencere3(QtWidgets.QWidget):
 
 
 
-app = QtWidgets.QApplication(sys.argv)
 
+app = QtWidgets.QApplication(sys.argv)
 pencere = Pencere()
 
-
 sys.exit(app.exec_())
+
+
+
+
